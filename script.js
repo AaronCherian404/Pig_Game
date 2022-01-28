bf73abaf642bc77049577081d52f81bd33069cde
Omnis404 <94530787+AaronCherian404@users.noreply.github.com>
2022-01-28 10:47:21 +0530

    Introduction added

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ let current0El = document.getElementById('current--0');
 let current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+
+const victory = document.querySelector('.Victory');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
 //starting values
 let playing, scores, currentScore, activePlayer;
 
@@ -31,6 +35,25 @@ const init = function () {
 };
 init();
 
+const messages = function (words) {
+  victory.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  document.querySelector('.victoryspeech').textContent = words;
+  const closeModal = function () {
+    victory.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+};
+messages(
+  'Welcome to the PIG GAME!!!\nFirst player to 100 points wins!!!\nMay the Best Player Win!!!'
+);
 //function to switch players
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -67,7 +90,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -75,25 +98,7 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
-      const victory = document.querySelector('.Victory');
-      const overlay = document.querySelector('.overlay');
-      const btnCloseModal = document.querySelector('.close-modal');
-      victory.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-      document.querySelector('.victoryspeech').textContent = `Player ${
-        Number(activePlayer) + 1
-      } is the winner`;
-      const closeModal = function () {
-        victory.classList.add('hidden');
-        overlay.classList.add('hidden');
-      };
-      btnCloseModal.addEventListener('click', closeModal);
-      overlay.addEventListener('click', closeModal);
-      document.addEventListener('keydown', function (e) {
-        if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-          closeModal();
-        }
-      });
+      messages(`Player ${Number(activePlayer) + 1} is the winner`);
     } else {
       switchPlayer();
     }
